9abe4db0f05b4a8af21e75a2bb4f911a84021a46
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2020-11-14 16:36:05 +0530

    API Route to book a ticket

---
+++ routes/book_ticket.js
@@ -0,0 +1,92 @@
+const route = require('express').Router()
+const Registered_Users = require('../db').Registered_Users
+const Show_Details = require('../db').Show_Details
+const Tickets_Sold = require('../db').Tickets_Sold
+
+route.post('/', (req, res) => {
+    const inpHour = req.body.time
+    const currDate = new Date().getTime();
+    const currHour = new Date().getHours();
+    const inpDate = new Date(req.body.date).getTime();
+    if(inpDate >= currDate) {
+        if((inpHour >= currHour) && ((inpHour == 12) || (inpHour == 15) || (inpHour == 18) || (inpHour == 21))) {
+            Registered_Users.findOne({
+                where: {name: req.body.name, phone: req.body.phone}
+            })
+            .then((user) => {
+                if(user !== null) {
+                    Show_Details.findOrCreate({
+                        where: {date: req.body.date, time: req.body.time},
+                        defaults: {
+                            ticket_count: 1
+                        }
+                    })
+                    .then(([show, created]) => {
+                        if(created === true) {
+                            // First Ticket Booking For This Show, Can Book A Ticket.
+                            Tickets_Sold.create({
+                                date: req.body.date,
+                                time: req.body.time,
+                                user_id: user.user_id
+                            })
+                            .then((booking) => {
+                                res.status(201).send(booking)
+                            })
+                            .catch((err) => {
+                                res.status(501).send("Couldn't Book Tickets.")
+                            })
+                        }
+                        else {
+                            // This Show Already Exists, So
+                            // First We Need To Check The Count Of Tickets, And 
+                            // We Can Book A Ticket Only If Count < 20.
+                            if(show.ticket_count < 20) {
+                                // After Booking The Ticket Also Increase The Count In Show Details
+                                Tickets_Sold.create({
+                                    date: req.body.date,
+                                    time: req.body.time,
+                                    user_id: user.user_id
+                                })
+                                .then((booking) => {
+                                    Show_Details.update({ ticket_count: (show.ticket_count + 1)}, {
+                                        where: {
+                                            date: req.body.date,
+                                            time: req.body.time      
+                                        }
+                                    })
+                                    res.status(201).send(booking)
+                                })
+                                .catch((err) => {
+                                    res.status(501).send("Couldn't Book Tickets.")
+                                })
+        
+                            } else {
+                                res.status(202).send("Show Already Booked Up ! Choose Some Other Timing.")
+                            }
+                        }
+                    })
+                    .catch((err) => {
+                        res.status(501).send({
+                            error: "Couldn't Check For Show Details."
+                        })
+                    })
+                }
+                else res.status(401).send("Unregistered User. Please Register Yourself First.")
+            })
+            .catch((err) => {
+                res.status(501).send({
+                    error: "Couldn't Check For Registered Users."
+                })
+            })
+        }
+        else {
+            res.status(501).send("Enter Valid Show Time & Try Again")
+        }
+    } else {
+        res.status(501).send("Enter Valid Date & Time For The Show & Try Again")
+    }
+})
+
+exports = module.exports = {
+    route
+}
