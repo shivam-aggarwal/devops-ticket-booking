b05736c27f7919e92e2f9589c3bf38f26c036967
shubhangi-mishra <74354873+shubhangi-mishra@users.noreply.github.com>
2020-11-21 21:39:07 +0530

    API Route to view all the shows.

---
+++ routes/all_shows.js
@@ -0,0 +1,20 @@
+const { Op } = require('sequelize')
+const route = require('express').Router()
+const Show_Details = require('../db').Show_Details
+
+// Send Details Of All Shows (Only For Testing Purpose)
+route.get('/', (req, res) => {
+    Show_Details.findAll({ where: {ticket_count: {[Op.ne]: 0}} })
+        .then((shows) => {
+            res.status(200).send(shows)
+        })
+        .catch((err) => {
+            res.status(500).send({
+                error: "Couldn't Retrieve Show Details"
+            })
+        })
+})
+
+exports = module.exports = {
+    route
+}
