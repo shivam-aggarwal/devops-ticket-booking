c141096f020f6af4a49c8723507a59b5819627d8
shivam-aggarwal <shivamaggarwal700@gmail.com>
2020-11-22 09:33:17 +0530

    Add method to auto delete old tickets

--- server.js
+++ server.js
@@ -21,6 +21,36 @@ server.use('/all_shows', require('./routes/all_shows').route)
 server.use('/all_tickets', require('./routes/all_tickets').route)
 server.use('/all_users', require('./routes/all_users').route)
 
+// Auto-Delete Old Tickets (8 Hours Old)
+schedule.scheduleJob('0 0 * * * *', () => {
+    var hr = new Date;
+    var hour = hr.getHours();
+
+    var today = new Date().toJSON().slice(0, 10)
+    var prevDate = new Date(today)
+    prevDate.setDate(prevDate.getDate() - 1)
+    prevDate = prevDate.toJSON().slice(0, 10)
+
+    if(hour == 20) { // For 12:00 Show
+        // Delete Shows Where Time == 12 On Same Date
+        Tickets_Sold.destroy({ where: { date: today, time: 12 } })
+        Show_Details.destroy({ where: { date: today, time: 12 } })
+    } else if (hour == 23) { // For 15:00 Show
+        // Delete Shows Where Time == 15 On Same Date
+        Tickets_Sold.destroy({ where: { date: today, time: 15 } })
+        Show_Details.destroy({ where: { date: today, time: 15 } })
+    } else if (hour == 2) { // For 18:00 Show
+        // Delete Shows Where Time == 18 On Previous Date
+        Tickets_Sold.destroy({ where: { date: prevDate, time: 18 } })
+        Show_Details.destroy({ where: { date: prevDate, time: 18 } })
+    } else if (hour == 5) { // For 21:00 Show
+        // Delete Shows Where Time == 21 On Previous Date
+        Tickets_Sold.destroy({ where: { date: prevDate, time: 21 } })
+        Show_Details.destroy({ where: { date: prevDate, time: 21 } })
+    } 
+})
+
+// Invalid API Request
 server.use((req, res) => res.send("404 Invalid API Call"))
 
 server.listen(1611, () => console.log("Server Started At : http://localhost:1611/"))
